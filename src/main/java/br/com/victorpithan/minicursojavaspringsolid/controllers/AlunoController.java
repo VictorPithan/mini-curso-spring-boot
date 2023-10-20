0b8f4339f5d30fe6bac7ff580c307b09dd9db9cd
VictorPithan <victorpithan@gmail.com>
2023-10-20 19:42:28 -0300

    feat:add aluno controller

---
+++ src/main/java/br/com/victorpithan/minicursojavaspringsolid/controllers/AlunoController.java
@@ -0,0 +1,12 @@
+package br.com.victorpithan.minicursojavaspringsolid.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/aluno")
+public class AlunoController {
+  
+  
+}
