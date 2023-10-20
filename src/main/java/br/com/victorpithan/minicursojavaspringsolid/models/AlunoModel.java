73f7220766adc45e13cb95a429cba8a9eb211705
VictorPithan <victorpithan@gmail.com>
2023-10-20 15:30:52 -0300

    feat:add aluno model

---
+++ src/main/java/br/com/victorpithan/minicursojavaspringsolid/models/AlunoModel.java
@@ -0,0 +1,45 @@
+package br.com.victorpithan.minicursojavaspringsolid.models;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "aluno")
+@ToString
+public class AlunoModel {
+
+  @Id
+  @Column
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  @Column(length = 100, nullable = false)
+  private String nome;
+
+  @Column(length = 100, nullable = false)
+  private String sobrenome;
+
+  @Column(length = 10, unique = true)
+  private String matricula;
+
+  @Column(length = 255, unique = true)
+  private String email;
+
+  @ManyToMany(mappedBy = "alunos")
+  @JsonIgnoreProperties("alunos")
+  private List<TurmaModel> turmas;
+}
