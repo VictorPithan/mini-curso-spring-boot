cc2c187f1c4ea27820ac27b96a1e292b165d288f
VictorPithan <victorpithan@gmail.com>
2023-10-20 20:47:04 -0300

    fix:fix name Aluno to AlunoModel in custom query

--- src/main/java/br/com/victorpithan/minicursojavaspringsolid/repositories/AlunoRepository.java
+++ src/main/java/br/com/victorpithan/minicursojavaspringsolid/repositories/AlunoRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.repository.query.Param;
 import br.com.victorpithan.minicursojavaspringsolid.models.AlunoModel;
 
 public interface AlunoRepository extends JpaRepository<AlunoModel, Integer> {
-  @Query("SELECT CASE WHEN COUNT(a) > 0 THEN true ELSE false END FROM Aluno a WHERE a.matricula = :matricula")
-    boolean existsByMatricula(@Param("matricula") String matricula);
+  @Query("SELECT CASE WHEN COUNT(a) > 0 THEN true ELSE false END FROM AlunoModel a WHERE a.matricula = :matricula")
+  boolean existsByMatricula(@Param("matricula") String matricula);
 }
