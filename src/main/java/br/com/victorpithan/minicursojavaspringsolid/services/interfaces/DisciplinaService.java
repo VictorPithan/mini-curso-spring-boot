8ae12ed159d01cd2c4c19dfd1b5ceaab77effdd6
VictorPithan <victorpithan@gmail.com>
2023-10-20 19:45:07 -0300

    feat:add interface disciplina service

---
+++ src/main/java/br/com/victorpithan/minicursojavaspringsolid/services/interfaces/DisciplinaService.java
@@ -0,0 +1,8 @@
+package br.com.victorpithan.minicursojavaspringsolid.services.interfaces;
+
+import br.com.victorpithan.minicursojavaspringsolid.dtos.DisciplinaDTO;
+import br.com.victorpithan.minicursojavaspringsolid.models.DisciplinaModel;
+
+public interface DisciplinaService extends CrudService<DisciplinaModel, DisciplinaDTO> {
+  
+}
