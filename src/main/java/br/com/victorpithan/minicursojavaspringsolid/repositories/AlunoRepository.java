1c6e524c5f6d25522bd59cd71c0cf0e42522d5b3
VictorPithan <victorpithan@gmail.com>
2023-10-20 19:41:43 -0300

    feat:add personal query to existsByMatricula

--- src/main/java/br/com/victorpithan/minicursojavaspringsolid/repositories/AlunoRepository.java
+++ src/main/java/br/com/victorpithan/minicursojavaspringsolid/repositories/AlunoRepository.java
@@ -1,9 +1,12 @@
 package br.com.victorpithan.minicursojavaspringsolid.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import br.com.victorpithan.minicursojavaspringsolid.models.AlunoModel;
 
 public interface AlunoRepository extends JpaRepository<AlunoModel, Integer> {
-  
+  @Query("SELECT CASE WHEN COUNT(a) > 0 THEN true ELSE false END FROM Aluno a WHERE a.matricula = :matricula")
+    boolean existsByMatricula(@Param("matricula") String matricula);
 }
