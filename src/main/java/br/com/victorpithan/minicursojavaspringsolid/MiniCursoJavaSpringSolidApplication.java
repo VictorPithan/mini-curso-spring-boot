006fbbc5a8c4a5c70f286e29c1a55e58a4aabddb
VictorPithan <victorpithan@gmail.com>
2023-10-20 15:06:26 -0300

    first commit

---
+++ src/main/java/br/com/victorpithan/minicursojavaspringsolid/MiniCursoJavaSpringSolidApplication.java
@@ -0,0 +1,13 @@
+package br.com.victorpithan.minicursojavaspringsolid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniCursoJavaSpringSolidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniCursoJavaSpringSolidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/victorpithan/minicursojavaspringsolid/MiniCursoJavaSpringSolidApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.victorpithan.minicursojavaspringsolid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniCursoJavaSpringSolidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
